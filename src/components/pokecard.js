a7562e455e80da3a7c46f5467eccb400767bc15f
omkar7750 <omkarkhenevar1@gmail.com>
2022-05-05 15:44:25 +0530

    Added testcases for PokeCard component

--- src/components/pokecard.js
+++ src/components/pokecard.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function Pokecard(props) {
   return (
-    <li className="li">
+    <li data-testid={`pokecard-${props.id}`} className="li">
       <figure className="pokimg">
-        <Link to={'/details/' + props.id}>
-          <img src={props.img} />
+        <Link  to={'/details/' + props.id}>
+          <img src={props.img} alt={`pok-image`} />
         </Link>
       </figure>
       <div className="pok-info">
-        <p className="pok-id">#{props.num}</p>
-        <h5 className="pok-name">{props.name}</h5>
+        <p data-testid="pokid" className="pok-id">#{props.num}</p>
+        <h5 data-testid="pokname" className="pok-name">{props.name}</h5>
         <div className="abilities">
           {props.type.map((t, tindex) => (
             <span
---
+++ src/test/pokecard.test.js
@@ -0,0 +1,76 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import PokeCard from '../components/pokecard';
+import { BrowserRouter } from 'react-router-dom';
+
+const samplePokmon = {
+    "id": 1,
+    "num": "001",
+    "name": "Bulbasaur",
+    "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "height": "0.71 m",
+    "weight": "6.9 kg",
+    "candy": "Bulbasaur Candy",
+    "candy_count": 25,
+    "egg": "2 km",
+    "spawn_chance": 0.69,
+    "avg_spawns": 69,
+    "spawn_time": "20:00",
+    "multipliers": [1.58],
+    "weaknesses": [
+      "Fire",
+      "Ice",
+      "Flying",
+      "Psychic"
+    ],
+    "next_evolution": [{
+      "num": "002",
+      "name": "Ivysaur"
+    }, {
+      "num": "003",
+      "name": "Venusaur"
+    }]
+  }
+
+describe("Test PokeCard component", () => {
+  test('renders PokeCard component', () => {
+    let component = render(<BrowserRouter><PokeCard {...samplePokmon } /></BrowserRouter>);
+    const linkElement = component.getByTestId(`pokecard-${samplePokmon.id}`); 
+    expect(linkElement).toBeInTheDocument();
+    
+  })
+
+  test('renders Pok image in the rendered component', () => {
+    let component = render(<BrowserRouter><PokeCard {...samplePokmon } /></BrowserRouter>);
+    const linkElement = component.getByAltText(`pok-image`); 
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('src', samplePokmon.img);
+  })
+
+  test('renders passed pok name', () => {
+    let component = render(<BrowserRouter><PokeCard {...samplePokmon } /></BrowserRouter>);
+    const linkElement = component.getByTestId(`pokname`); 
+    expect(linkElement.textContent).toEqual(samplePokmon.name);
+    
+  })
+
+  test('renders passed pok number', () => {
+    let component = render(<BrowserRouter><PokeCard {...samplePokmon } /></BrowserRouter>);
+    const linkElement = component.getByTestId(`pokid`); 
+    expect(linkElement.textContent).toEqual(`#${samplePokmon.num}`);
+  })
+
+  test('renders pok types for current pokemon', () => {
+    let component = render(<BrowserRouter><PokeCard {...samplePokmon } /></BrowserRouter>);
+    const linkElement1 = component.getByText(samplePokmon.type[0]); 
+    expect(linkElement1).toBeInTheDocument();
+
+    const linkElement2 = component.getByText(samplePokmon.type[1]); 
+    expect(linkElement2).toBeInTheDocument();
+  })
+  
+})
