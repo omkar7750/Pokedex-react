3f5e475a49d1999fcaeaad268e3fc0c0d52462e4
omkar7750 <omkarkhenevar1@gmail.com>
2022-05-05 19:51:32 +0530

    Added some test cases for PokeDex component

--- src/test/pokedex.test.js
+++ src/test/pokedex.test.js
@@ -1,12 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import Pokedex from '../components/Pokedex';
+import { BrowserRouter } from 'react-router-dom';
 
 
 describe("Test Pokedex component", () => {
   test('renders Pokedex component', () => {
     render(<Pokedex />);
-    const linkElement = screen.getByText(/Pokedex/i);
+    const linkElement = screen.getByText(/Pokédex/i);
     expect(linkElement).toBeInTheDocument();
   });
 
@@ -22,10 +23,74 @@ describe("Test Pokedex component", () => {
     expect(AdvSearchComp).toBeInTheDocument();
   })
 
-  test('render Pok list container', () => {
-    const component = render(<Pokedex />);
-    const AdvSearchComp = component.getByTestId('pokListContainer');
-    expect(AdvSearchComp).toBeInTheDocument();
+  test('render Pok list container', async() => {
+    const component = render(<BrowserRouter><Pokedex /></BrowserRouter>);
+    await waitFor(() =>{ 
+      expect(component.getAllByAltText('pok-image')).toHaveLength(151);
+    })
+    
+  })
+
+  test('Test searchbox filtering', async() => {
+    const component = render(<BrowserRouter><Pokedex /></BrowserRouter>);
+    
+    await waitFor(() =>{
+      const inputCtrl = component.getByTestId('searchbox-input');
+      fireEvent.change(inputCtrl, {target: {value: 'char'}});
+      const searchboxSearchbtn = component.getByTestId('searchboxSearchbtn');
+      fireEvent.click(searchboxSearchbtn);
+      expect(component.getAllByAltText('pok-image')).toHaveLength(3);
+
+    });
+  })
+
+  test('Test advanced search and reset functionality', async() => {
+    const component = render(<BrowserRouter><Pokedex /></BrowserRouter>);
+  
+    await waitFor(() =>{
+      const advSearchAccordion = component.getByTestId('advancedSearch');
+      fireEvent.click(advSearchAccordion);
+      fireEvent.click(component.getByTestId('type-Flying'));
+      fireEvent.click(component.getByTestId('type-Fire'));
+      fireEvent.click(component.getByTestId('advSearchBtn'));
+      expect(component.getAllByAltText('pok-image')).toHaveLength(2);
+
+      fireEvent.click(component.getByTestId('resetBtn'));
+      expect(component.getAllByAltText('pok-image')).toHaveLength(151);
+    });
+  })
+
+  test('Test combination of adv. search , reset and searchbox filter', async() => {
+    const component = render(<BrowserRouter><Pokedex /></BrowserRouter>);
+  
+    await waitFor(() =>{
+
+      const inputCtrl = component.getByTestId('searchbox-input');
+      fireEvent.change(inputCtrl, {target: {value: 'char'}});
+      const searchboxSearchbtn = component.getByTestId('searchboxSearchbtn');
+      fireEvent.click(searchboxSearchbtn);
+      expect(component.getAllByAltText('pok-image')).toHaveLength(3);
+
+      const advSearchAccordion = component.getByTestId('advancedSearch');
+      fireEvent.click(advSearchAccordion);
+      fireEvent.click(component.getByTestId('type-Flying'));
+      fireEvent.click(component.getByTestId('type-Fire'));
+      fireEvent.click(component.getByTestId('advSearchBtn'));
+      expect(component.getAllByAltText('pok-image')).toHaveLength(1);
+
+      fireEvent.change(inputCtrl, {target: {value: ''}});
+      fireEvent.click(searchboxSearchbtn);
+      expect(component.getAllByAltText('pok-image')).toHaveLength(2);
+
+      fireEvent.change(inputCtrl, {target: {value: 'char'}});
+      fireEvent.click(searchboxSearchbtn);
+      fireEvent.click(component.getByTestId('resetBtn'));
+      expect(component.getAllByAltText('pok-image')).toHaveLength(3);
+
+      fireEvent.change(inputCtrl, {target: {value: ''}});
+      fireEvent.click(searchboxSearchbtn);
+      expect(component.getAllByAltText('pok-image')).toHaveLength(151);
+    });
   })
 
 })
