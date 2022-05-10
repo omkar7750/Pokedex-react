6467ff9577d9e61d80576a16533cf5d3dd14394a
omkar7750 <omkarkhenevar1@gmail.com>
2022-05-10 21:15:54 +0530

    Added enzyme test cases for advanced search component

--- src/test/advancedsearch.test.js
+++ src/test/advancedsearch.test.js
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
+// import { fireEvent, render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom'
+import { mount } from "enzyme";
 import AdvancedSearch from '../components/advancedsearch';
 import 'whatwg-fetch'
 
@@ -12,71 +13,139 @@ const backgroundColor = (element) => window.getComputedStyle(element).background
 
 describe('Test Advanced search component', ()=>{
     test('renders PokeCard component', () => {
-        let component = render(<AdvancedSearch {...sampleProps} />);
-        const advSearchComp = component.getByTestId(`advancedSearch`); 
-        expect(advSearchComp).toBeInTheDocument();
+
+        const wrapper =mount(<AdvancedSearch {...sampleProps} />);
+        const AdvancedSearchComp = wrapper.find('AdvancedSearch');
+        expect(AdvancedSearchComp.find(`[data-testid=\"advancedSearch\"]`)).toBeTruthy() ;
+        expect(AdvancedSearchComp.find('Accordion')).toHaveLength(1)
+
+
+        /** Using react testing library */
+        // let component = render(<AdvancedSearch {...sampleProps} />);
+        // const advSearchComp = component.getByTestId(`advancedSearch`); 
+        // expect(advSearchComp).toBeInTheDocument();
     })
 
     test('test all abilities rendered', () => {
-        let component = render(<AdvancedSearch {...sampleProps} />);
-        const advSearchComp = component.getByTestId(`advancedSearch`); 
-        fireEvent.click(advSearchComp);
-        let abilitiesComps = component.getAllByTestId('ability');
+
+        const wrapper =mount(<AdvancedSearch {...sampleProps} />);
+        const AdvancedSearchComp = wrapper.find('AdvancedSearch');
+        AdvancedSearchComp.find('Accordion').simulate('click');
+        let abilitiesComps = AdvancedSearchComp.childAt(0).find('.content-block-full').find('li');
         expect(abilitiesComps.length).toEqual(sampleProps.typeWeaknessList.length);
+
+        /** Using react testing library */
+        // let component = render(<AdvancedSearch {...sampleProps} />);
+        // const advSearchComp = component.getByTestId(`advancedSearch`); 
+        // fireEvent.click(advSearchComp);
+        // let abilitiesComps = component.getAllByTestId('ability');
+        // expect(abilitiesComps.length).toEqual(sampleProps.typeWeaknessList.length);
     })
 
     test('render search and reset buttons', () => {
-        const component = render(<AdvancedSearch {...sampleProps} />);
-        const advSearchComp = component.getByTestId(`advancedSearch`); 
-        fireEvent.click(advSearchComp);
-        const  advSearchBtn = component.getByTestId('advSearchBtn');
-        const resetBtn = component.getByTestId('resetBtn');
-        expect(advSearchBtn).toBeInTheDocument();
-        expect(resetBtn).toBeInTheDocument();
+
+        const wrapper =mount(<AdvancedSearch {...sampleProps} />);
+        const AdvancedSearchComp = wrapper.find('AdvancedSearch');
+        AdvancedSearchComp.find('Accordion').simulate('click');
+        expect(AdvancedSearchComp.childAt(0).find('AccordionItemPanel').find('.mresetbtnadv')).toHaveLength(1);
+        expect(AdvancedSearchComp.childAt(0).find('AccordionItemPanel').find('.msearchbtnadv')).toHaveLength(1);
+        
+
+        /** Using react testing library */
+        // const component = render(<AdvancedSearch {...sampleProps} />);
+        // const advSearchComp = component.getByTestId(`advancedSearch`); 
+        // fireEvent.click(advSearchComp);
+        // const  advSearchBtn = component.getByTestId('advSearchBtn');
+        // const resetBtn = component.getByTestId('resetBtn');
+        // expect(advSearchBtn).toBeInTheDocument();
+        // expect(resetBtn).toBeInTheDocument();
     })
 
     test('Test type and filter selectors length and are clickable', () => {
-        const component = render(<AdvancedSearch {...sampleProps} />);
-        const advSearchComp = component.getByTestId(`advancedSearch`); 
-        fireEvent.click(advSearchComp);
-        const  typeSelectorBtn = component.getAllByTestId(/type-/);
-        const  weaknessSelectorBtn = component.getAllByTestId(/weakness-/);
+
+        const wrapper =mount(<AdvancedSearch {...sampleProps} />);
+        const AdvancedSearchComp = wrapper.find('AdvancedSearch');
+        AdvancedSearchComp.find('Accordion').simulate('click');
+
+        let typeSelectorBtn = AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter');
+        let weaknessSelectorBtn = AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.weakness-filter');        
         expect(typeSelectorBtn.length).toEqual(sampleProps.typeWeaknessList.length);
         expect(weaknessSelectorBtn.length).toEqual(sampleProps.typeWeaknessList.length);
 
-        fireEvent.click(typeSelectorBtn[0]);
-        expect(backgroundColor(typeSelectorBtn[0])).toBe("rgb(48, 167, 215)");
+        
+        AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter').at(0).simulate('click');
+        // console.log(typeSelectorBtn.at(0).prop('style'), typeSelectorBtn.at(0).html())
+        expect(AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter').at(0).props().style.backgroundColor).toBe("#f2f2f2");
 
-        fireEvent.click(typeSelectorBtn[0]);
-        expect(backgroundColor(typeSelectorBtn[0])).toBe("rgb(242, 242, 242)");
+        AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter').at(0).simulate('click');
+        console.log(AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter').at(0).prop('style'), AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter').at(0).html())
+        expect(AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter').at(0).props().style.backgroundColor).toBe("#f2f2f2");
+
+        /** Using react testing library */
+        // const component = render(<AdvancedSearch {...sampleProps} />);
+        // const advSearchComp = component.getByTestId(`advancedSearch`); 
+        // fireEvent.click(advSearchComp);
+        // const  typeSelectorBtn = component.getAllByTestId(/type-/);
+        // const  weaknessSelectorBtn = component.getAllByTestId(/weakness-/);
+        // expect(typeSelectorBtn.length).toEqual(sampleProps.typeWeaknessList.length);
+        // expect(weaknessSelectorBtn.length).toEqual(sampleProps.typeWeaknessList.length);
+
+        // fireEvent.click(typeSelectorBtn[0]);
+        // expect(backgroundColor(typeSelectorBtn[0])).toBe("rgb(48, 167, 215)");
+
+        // fireEvent.click(typeSelectorBtn[0]);
+        // expect(backgroundColor(typeSelectorBtn[0])).toBe("rgb(242, 242, 242)");
         
     });
 
 
     test('Passed handleMSearch gets called on search click', () => {
         
-        const component = render(<AdvancedSearch {...sampleProps} />);
-        const  advSearchBtn = component.getByTestId('advSearchBtn');
-        const  typeSelectorBtn = component.getAllByTestId(/type-/);
-        const  weaknessSelectorBtn = component.getAllByTestId(/weakness-/);
-        fireEvent.click(advSearchBtn); 
+        const wrapper =mount(<AdvancedSearch {...sampleProps} />);
+        const AdvancedSearchComp = wrapper.find('AdvancedSearch');
+        AdvancedSearchComp.find('Accordion').simulate('click');
+        const advSearchBtn = AdvancedSearchComp.childAt(0).find('AccordionItemPanel').find('.msearchbtnadv');
+        let typeSelectorBtn = AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.type-filter');
+        let weaknessSelectorBtn = AdvancedSearchComp.childAt(0).find('.content-block-full').find('span.weakness-filter');   
+        
+        advSearchBtn.simulate('click');
         expect(sampleProps.handleMSearch).toHaveBeenCalledWith("", [], []);
-
-
-        fireEvent.click( typeSelectorBtn[0]);
-        fireEvent.click( weaknessSelectorBtn[4]);
-        fireEvent.click(advSearchBtn); 
+        typeSelectorBtn.at(0).simulate('click');
+        weaknessSelectorBtn.at(4).simulate('click');
+        advSearchBtn.simulate('click');
         expect(sampleProps.handleMSearch).toHaveBeenCalledWith( "", [sampleProps.typeWeaknessList[0]], [sampleProps.typeWeaknessList[4]]);
+
+        /** Using react testing library */
+        // const component = render(<AdvancedSearch {...sampleProps} />);
+        // const  advSearchBtn = component.getByTestId('advSearchBtn');
+        // const  typeSelectorBtn = component.getAllByTestId(/type-/);
+        // const  weaknessSelectorBtn = component.getAllByTestId(/weakness-/);
+        // fireEvent.click(advSearchBtn); 
+        // expect(sampleProps.handleMSearch).toHaveBeenCalledWith("", [], []);
+
+
+        // fireEvent.click( typeSelectorBtn[0]);
+        // fireEvent.click( weaknessSelectorBtn[4]);
+        // fireEvent.click(advSearchBtn); 
+        // expect(sampleProps.handleMSearch).toHaveBeenCalledWith( "", [sampleProps.typeWeaknessList[0]], [sampleProps.typeWeaknessList[4]]);
 
     })
 
     
     test('Passed handleMSearch gets called on reset click', () => {
         
-        const component = render(<AdvancedSearch {...sampleProps} />);
-        const  resetBtn = component.getByTestId('resetBtn');
-        fireEvent.click(resetBtn); 
-        expect(sampleProps.handleMSearch).toHaveBeenCalledWith("", "", "");
+        const wrapper =mount(<AdvancedSearch {...sampleProps} />);
+        const AdvancedSearchComp = wrapper.find('AdvancedSearch');
+        AdvancedSearchComp.find('Accordion').simulate('click');
+        const  resetBtn = AdvancedSearchComp.childAt(0).find('AccordionItemPanel').find('.msearchbtnadv');
+        resetBtn.simulate('click');
+        expect(sampleProps.handleMSearch).toHaveBeenCalledWith("", [], []);
+
+        /** Using react testing library */
+        // const component = render(<AdvancedSearch {...sampleProps} />);
+        // const  resetBtn = component.getByTestId('resetBtn');
+        // fireEvent.click(resetBtn); 
+        // expect(sampleProps.handleMSearch).toHaveBeenCalledWith("", "", "");
     })
 
 })
