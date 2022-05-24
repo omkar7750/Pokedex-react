00fcbb8e73e09bbd2a0b2f84bc2f8c5339fdb155
omkar7750 <omkarkhenevar1@gmail.com>
2022-05-24 21:49:57 +0530

    responsive css

--- src/components/floatingnavbtn.jsx
+++ src/components/floatingnavbtn.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function FloatingNavButton(props) {
-  return (<Link to={'/'}><div className='nav-btn'></div></Link>)
+  return (<Link className='nav-btn-link' to={'/'}><div className='nav-btn'></div></Link>)
 }
