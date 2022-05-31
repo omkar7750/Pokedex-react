9ce7f2e3be814b49507fbd6c324f08c8d4a37f86
omkar7750 <omkarkhenevar1@gmail.com>
2022-05-31 20:18:51 +0530

    Added gallary component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Pokedex from './components/Pokedex';
 import PokDetails from './components/pokdetails';
 import PokHome from './components/pokhome';
 import AboutUs from './components/aboutus';
+import Gallary from './components/gallary';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -16,6 +18,7 @@ export default function App() {
           <Route path="/pokdex" exact element={<Pokedex />}></Route>
           <Route path="/details/:pokId" exact element={<PokDetails />}></Route>
           <Route path="/aboutus" exact element={<AboutUs />}></Route>
+          <Route path="/gallary" exact element={<Gallary />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/gallary.jsx
@@ -0,0 +1,56 @@
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+import config from '../config';
+import Loader from './loader';
+
+export default function Gallary(props) { 
+    const [imagePath, setImagePath] = useState("")
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        fetch(config.dataUrl)
+            .then((response) => response.json())
+            .then(async(json) => {
+                let images = json.pokemon.map((pok) => pok.img);
+                setImages(images);
+                setImagePath(images[0])
+            })
+    }, [])
+
+    const viewImage = (img) => {
+        setImagePath(img);
+    }
+
+    const viewPrevious = () => {
+        const imageIndex = images.indexOf(imagePath);
+        if(imageIndex == 0) return;
+        setImagePath(images[imageIndex - 1])
+    }
+
+    const viewNext = () => {
+        const imageIndex = images.indexOf(imagePath);
+        if(imageIndex == images.length - 1) return;
+        setImagePath(images[imageIndex + 1])
+    }
+
+    return (
+        <div className='gallary-container'>
+        <div className='gallary'>
+            <div className='gal-heading'>Pokemon Gallary</div>
+            <div className='gal-zoomed-view'>{imagePath ?
+                <>
+                    <FontAwesomeIcon className={'gal-image-slide-icon ' + (imagePath == images[0]? "disabled" : "")} icon={faChevronLeft} onClick={viewPrevious} />
+                    <img src={imagePath} className="zoomed-image" />
+                    <FontAwesomeIcon className={'gal-image-slide-icon ' + (imagePath == images[images.length - 1]? "disabled" : "")} icon={faChevronRight} onClick={viewNext} />
+                </>: <Loader /> }
+            </div>
+            <div className='gal-images-to-view'>{
+                images.map((img, i) => {
+                    return (<img src={img} key={`imgview-${i}`} onClick={() => viewImage(img)} className={"imgview " + (img == imagePath? "gal-img-active": "")} />)
+                })
+            }</div>
+        </div>
+        </div>
+    )
+}
