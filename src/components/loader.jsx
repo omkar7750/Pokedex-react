a04af0b84aabc9ecc538a89a749eb056db22c07e
omkar7750 <omkarkhenevar1@gmail.com>
2022-05-31 13:25:05 +0530

    replaced image in loader component with Loading text

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import loaderImage from '../utility/images/loader1.gif';
+// import loaderImage from '../utility/images/loader1.gif';
 
 export default function Loader(props) {
-    return <><img className='loader-comp' src = {loaderImage} /></>
+    // return <><img className='loader-comp' src = {loaderImage} /></>
+    return <div className='loader-comp' >Loading ...</div>
     
 }
