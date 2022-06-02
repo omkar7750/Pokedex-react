0eeded95f8f85b3d7b24b21400411ff91cf523ac
omkar7750 <omkarkhenevar1@gmail.com>
2022-06-02 15:49:03 +0530

    css changes

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 // import loaderImage from '../utility/images/loader1.gif';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSpinner} from '@fortawesome/free-solid-svg-icons';
 
 export default function Loader(props) {
     // return <><img className='loader-comp' src = {loaderImage} /></>
-    return <div className='loader-comp' >Loading ...</div>
+    return <div className='loader-comp' ><FontAwesomeIcon className='spinner' icon={faSpinner} /></div>
     
 }
